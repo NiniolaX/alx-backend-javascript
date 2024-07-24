d13d69701a512bd104fda7853dd4039006230e08
NiniolaX <loveafinni16@gmail.com>
2024-07-24 16:33:54 +0100

    Corrected code to achieve task objective

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,15 +1,15 @@
 /*
-Modified variables (formerly all declared with 'var') so they aren't
+Modified variables (all formerly declared with 'var') so they aren't
 overwritten inside the conditional block
 */
 export default function taskBlock(trueOrFalse) {
-    const task = false;
-    const task2 = true;
-  
-    if (trueOrFalse) {
-      let task = true;
-      let task2 = false;
-    }
-  
-    return [task, task2];
+  const task = false;
+  const task2 = true;
+
+  if (trueOrFalse) {
+    const task = true;
+    const task2 = false;
+  }
+
+  return [task, task2];
 }
