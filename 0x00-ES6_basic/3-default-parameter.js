5790ce820829bc74aacab02db9c4eebcd8e58e0a
NiniolaX <loveafinni16@gmail.com>
2024-07-24 17:59:19 +0100

    Solved task 4 and made addition to task 3 Task 3: Added default value for all function parameters

--- 0x00-ES6_basic/3-default-parameter.js
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -2,7 +2,7 @@
 Condensed the internals of the *initial function* to 1 line - without changing the name of
 each function/variable.
 */
-export default function getSumOfHoods(initialNumber, expansion1989 = 89, expansion2019 = 19) {
+export default function getSumOfHoods(initialNumber = 0, expansion1989 = 89, expansion2019 = 19) {
   return initialNumber + expansion1989 + expansion2019;
 }
 
---
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -0,0 +1,15 @@
+/*
+Modified initial function to return the number of arguments passed to it using the rest parameter
+syntax
+*/
+export default function returnHowManyArguments(...restParams) {
+  return restParams.length;
+}
+
+/*
+INITIAL FUNCTION:
+
+export default function returnHowManyArguments() {
+
+}
+*/
