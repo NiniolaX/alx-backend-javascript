b39b201656cf73e8f23f658fc6f8500480e4f454
NiniolaX <loveafinni16@gmail.com>
2024-07-24 18:31:02 +0100

    Solved task 5

---
+++ 0x00-ES6_basic/5-spread-operator.js
@@ -0,0 +1,14 @@
+/*
+Concatenated 2 arrays and each character of a string by modifying the
+initial function, using spread syntax
+*/
+export default function concatArrays(array1, array2, string) {
+  return [...array1, ...array2, ...string];
+}
+
+/*
+INITIAL FUNCTION:
+
+export default function concatArrays(array1, array2, string) {
+}
+*/
