7035089099cf6ffb380c1332cd7cda975d124251
NiniolaX <loveafinni16@gmail.com>
2024-07-24 18:46:00 +0100

    Solved task 6

---
+++ 0x00-ES6_basic/6-string-interpolation.js
@@ -0,0 +1,28 @@
+// Rewrote return statement to use template literals for simpler substitutions of defined variables
+export default function getSanFranciscoDescription() {
+  const year = 2017;
+  const budget = {
+    income: '$119,868',
+    gdp: '$154.2 billion',
+    capita: '$178,479',
+  };
+
+  return `As of ${year}, it was the seventh-highest income county in the United States, with a per capita personal income of ${budget.income}. As of 2015, San Francisco proper had a GDP of ${budget.gdp}, and a GDP per capita of ${budget.capita}.`;
+}
+
+/*
+INITIAL FUNCTION:
+
+export default function getSanFranciscoDescription() {
+  const year = 2017;
+  const budget = {
+    income: '$119,868',
+    gdp: '$154.2 billion',
+    capita: '$178,479',
+  };
+
+  return 'As of ' + year + ', it was the seventh-highest income county in the United States'
+        / ', with a per capita personal income of ' + budget.income + '. As of 2015, San Francisco'
+        / ' proper had a GDP of ' + budget.gdp + ', and a GDP per capita of ' + budget.capita + '.';
+}
+*/
