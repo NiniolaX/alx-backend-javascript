762035df193b9bdccc1d438ef564054940798767
NiniolaX <loveafinni16@gmail.com>
2024-07-26 17:55:52 +0100

    Solved task 0

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,11 @@
+export default function getResponseFromAPI() {
+  const promise = new Promise(((resolve, reject) => {
+    const userData = true; // Simulate a network request
+    if (userData) {
+      resolve(userData);
+    } else {
+      reject(new Error('Error fetching user data'));
+    }
+  }));
+  return promise;
+}
