17391221a7efa18dac13c8f94fcd9793f94b31f0
NiniolaX <loveafinni16@gmail.com>
2024-07-27 23:48:31 +0100

    Modified script to meet requirements

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -7,5 +7,11 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   const uploadPhotoPromise = uploadPhoto(fileName);
 
   return Promise.allSettled([signUpUserPromise, uploadPhotoPromise])
-    .then((results) => results);
+    .then((results) => results.map((result) => {
+      if (result.status === 'rejected') {
+        // Modify result on rejection to have a 'status' instead of 'reason' as required
+        return { status: 'rejected', value: result.reason };
+      }
+      return result; // Return result for fulfillment as is
+    }));
 }
