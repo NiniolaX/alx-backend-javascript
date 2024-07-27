7b2d855adb150bbc3be8859b1c82a6220cb09842
NiniolaX <loveafinni16@gmail.com>
2024-07-27 23:19:48 +0100

    Solved task 7: Wrote function that calls two promises and returns a promise when all promises have been settled

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,11 @@
+//
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const signUpUserPromise = signUpUser(firstName, lastName);
+  const uploadPhotoPromise = uploadPhoto(fileName);
+
+  return Promise.allSettled([signUpUserPromise, uploadPhotoPromise])
+    .then((results) => results);
+}
