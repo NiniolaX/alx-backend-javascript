b56743ee074d2cc0921b3467924a143acc334487
NiniolaX <loveafinni16@gmail.com>
2024-07-27 20:57:05 +0100

    Updated function to return promise

--- 0x01-ES6_promise/2-main.js
+++ 0x01-ES6_promise/2-main.js
@@ -1,4 +1,5 @@
-import handleResponseFromAPI from "./2-then";
+import handleResponseFromAPI from "./2-then.js";
 
 const promise = Promise.resolve();
 handleResponseFromAPI(promise);
+console.log(promise instanceof Promise);
--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -3,9 +3,8 @@ Function takes in a promise as argument and appends three handlers to
 the promise based on how promise was settled
 */
 export default function handleResponseFromAPI(promise) {
-  promise.then(() => ({ status: 200, body: 'success' }))
-    .catch(() => Error())
-    .finally(() => {
-      console.log('Got a response from the API');
-    });
+  return promise
+    .then(() => ({ status: 200, body: 'success' }))
+    .catch(() => (new Error()))
+    .finally(() => console.log('Got a response from the API'));
 }
