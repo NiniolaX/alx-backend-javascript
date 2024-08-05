cd5c377a73d67a7c30894baecb0ac938a416cc8d
NiniolaX <loveafinni16@gmail.com>
2024-08-05 12:39:55 +0100

    Solved task 9

---
+++ 0x01-ES6_promise/9-try.js
@@ -0,0 +1,13 @@
+// Function appends an item to a function based on the result of a math function passed as argument
+export default function guardrail(mathFunction) {
+  const queue = [];
+  try {
+    const result = mathFunction();
+    queue.push(result);
+  } catch (error) {
+    queue.push(error.message);
+  } finally {
+    queue.push('Guardrail was processed');
+  }
+  return queue;
+}
