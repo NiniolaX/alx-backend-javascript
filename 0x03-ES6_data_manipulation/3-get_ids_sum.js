9c6d544739b5ea2350b316322f4382ca68ec3793
NiniolaX <loveafinni16@gmail.com>
2024-08-08 09:38:49 +0100

    Modified file to meet eslint requirements

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,5 +1,5 @@
 // Function returns sum of all the student IDs.
 export default function getStudentIdsSum(studentsList) {
-    const sumOfIds = studentsList.map((student) => student.id).reduce((total, id) => total + id);
-    return sumOfIds;
+  const sumOfIds = studentsList.map((student) => student.id).reduce((total, id) => total + id);
+  return sumOfIds;
 }
