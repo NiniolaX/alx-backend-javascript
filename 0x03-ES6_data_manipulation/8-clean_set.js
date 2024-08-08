31eada6f9850fb5b5e55e39928adbf58969fbf5e
NiniolaX <loveafinni16@gmail.com>
2024-08-08 23:38:24 +0100

    Updated cleanSet function to handle base cases: - Empty string - Non-string data argument - Missing string argument

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,10 +1,10 @@
 // Function returns a string of all the set values that start with a specific string
 export default function cleanSet(set, startString) {
-  if (startString === '') {
+  if (!startString || (typeof startString !== 'string')) {
     return '';
   }
-  return Array.from(set)
-    .filter((element) => element.startsWith(startString))
-    .map((element) => element.slice(3))
+  return Array.from(set) // Create an array from set
+    .filter((element) => element && element.startsWith(startString)) // also skip empty elements
+    .map((element) => element.slice(startString.length))
     .join('-');
 }
--- 0x03-ES6_data_manipulation/8-main.js
+++ 0x03-ES6_data_manipulation/8-main.js
@@ -1,4 +1,7 @@
 import cleanSet from "./8-clean_set.js";
 
-console.log(cleanSet(new Set(['bonjovi', 'bonaparte', 'bonappetit', 'banana']), 'bon'));
-console.log(cleanSet(new Set(['bonjovi', 'bonaparte', 'bonappetit', 'banana']), ''));
+console.log(cleanSet(new Set(['bonjovi0', 'bonaparte', 'bonappetit', 'banana']), 'bon')); // non-empty string
+console.log(cleanSet(new Set(['bonjovi1', 'bonaparte', 'bonappetit', 'banana']), '')); // empty string
+console.log(cleanSet(new Set(['id-test', 'id-chicken', 'id-user', , 'id-id-']), 'id-')); // non-empty string
+console.log(cleanSet(new Set(['bonjovi2', 'bonaparte', 'bonappetit', 'banana']))); // empty string
+console.log(cleanSet(new Set(['bonjovi3', 'bonaparte', 'bonappetit', 'banana']), [])); // empty string
