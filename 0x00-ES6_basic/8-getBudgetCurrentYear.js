36fde02445f3209106605e29a02951922cbd03ec
NiniolaX <loveafinni16@gmail.com>
2024-10-11 17:27:51 +0100

    Updated codebase to meet task requiremnts

--- 0x00-ES6_basic/8-getBudgetCurrentYear.js
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -13,22 +13,3 @@ export default function getBudgetForCurrentYear(income, gdp, capita) {
 
   return budget;
 }
-
-/*
-INITIAL CODE:
-
-function getCurrentYear() {
-  const date = new Date();
-  return date.getFullYear();
-}
-
-export default function getBudgetForCurrentYear(income, gdp, capita) {
-  const budget = {};
-
-  budget[`income-${getCurrentYear()}`] = income;
-  budget[`gdp-${getCurrentYear()}`] = gdp;
-  budget[`capita-${getCurrentYear()}`] = capita;
-
-  return budget;
-}
-*/
