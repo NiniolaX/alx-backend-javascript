8970065d5a560b2978ea448944755f2669f8c483
NiniolaX <loveafinni16@gmail.com>
2024-11-26 14:04:21 +0100

    Fixed errors

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -8,41 +8,47 @@
 const fs = require('fs');
 
 function countStudents(path) {
-  return fs.readFile(path, 'utf-8', (err, data) => {
-    if (err) {
-        console.log('Cannot load the database');
-        return;
-    }
-    // Split file into rows and remove empty rows
-    const rows = data.split('\n').filter((row) => row.trim() !== '');
-
-    // Remove header
-    rows.shift();
-
-    // Log total number of students
-    console.log(`Number of students: ${rows.length}`);
-
-    // Object to hold students by field
-    const studentsByField = {};
-
-    for (const row of rows) {
-      if (row.length > 0) {
-        const [firstName, lastName, age, field] = row.trim().split(',');
-
-        // Skip invalid rows
-        if (firstName && lastName && age && field) {
-          if (!(field in studentsByField)) {
-            studentsByField[field] = [];
-          }
-          studentsByField[field].push(firstName);
+  const promise = new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf-8', (err, data) => {
+        if (err) {
+            reject(Error('Cannot load the database'));
+            return;
         }
-      }
-    }
+        // Split file into rows and remove empty rows
+        const rows = data.split('\n').filter((row) => row.trim() !== '');
 
-    for (const [field, names] of Object.entries(studentsByField)) {
-      console.log(`Number of students in ${field}: ${names.length}. List: ${names.join(', ')}`);
-    }
-  });
+        // Remove header
+        rows.shift();
+
+        // Log total number of students
+        console.log(`Number of students: ${rows.length}`);
+
+        // Object to hold students by field
+        const studentsByField = {};
+
+        for (const row of rows) {
+          if (row.length > 0) {
+            const [firstName, lastName, age, field] = row.trim().split(',');
+
+            // Skip invalid rows
+            if (firstName && lastName && age && field) {
+              if (!(field in studentsByField)) {
+                studentsByField[field] = [];
+              }
+              studentsByField[field].push(firstName);
+            }
+          }
+        }
+
+        for (const [field, names] of Object.entries(studentsByField)) {
+          console.log(`Number of students in ${field}: ${names.length}. List: ${names.join(', ')}`);
+        }
+
+        resolve();
+      });
+    });
+
+    return promise;
 }
 
 module.exports = countStudents;
