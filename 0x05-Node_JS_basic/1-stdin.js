add498b16e72902149e98fd62bc2fd021bd7c104
Love Afinni <loveafinni16@gmail.com>
2024-11-26 16:28:17 +0000

    Added program that interacts with user on terminal

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,16 @@
+/*
+ * Simple STDIN interaction program
+ */
+proces = require('process');
+
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+process.stdin.on('data', (input) => {
+  userInput = input.toString().trim();
+  process.stdout.write(`Your name is: ${input}`);
+});
+
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
+  process.exit();
+});
