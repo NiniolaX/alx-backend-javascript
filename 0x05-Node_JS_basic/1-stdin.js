5a93aed3dbd2ef3c3aac7278d1a10077006d739c
NiniolaX <loveafinni16@gmail.com>
2024-11-27 06:58:58 +0100

    Updated script to be used in old mode

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -2,6 +2,7 @@ require('process');
 
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.setEncoding('utf-8');
+process.stdin.resume();
 
 process.stdin.on('readable', () => {
   const name = process.stdin.read();
