0d2406b81dab7c760ea9b474e2c9810c7f9cbb6d
NiniolaX <loveafinni16@gmail.com>
2024-11-27 06:54:10 +0100

    Used readable event instead of data

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,13 +1,15 @@
 require('process');
 
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdin.setEncoding('utf-8');
 
-process.stdin.on('data', (input) => {
-  const userInput = input.toString().trim();
-  process.stdout.write(`Your name is: ${userInput}\n`);
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  if (name != null) {
+    process.stdout.write(`Your name is: ${name.toString().trim()}\n`);
+  }
 });
 
 process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
-  process.exit();
 });
