781c9705d1d0b1a34ef5893868d563750d64c351
NiniolaX <loveafinni16@gmail.com>
2024-11-29 17:03:21 +0100

    Task 2: Added test file that uses Chai

--- 0x06-unittests_in_js/2-calcul.test.js
+++ 0x06-unittests_in_js/2-calcul.test.js
@@ -1,76 +1,77 @@
-const assert = require('assert');
+const chai = require('chai');
+const expect = chai.expect;
 const calculateNumber = require('./1-calcul');
 
 describe('#calculateNumber() with SUM', () => {
   it('calculates sum of two positive numbers', () => {
-    expect(calculateNumber('SUM', 1.8, 2.0), 4);
+    expect(calculateNumber('SUM', 1.8, 2.0)).to.equal(4);
   });
 
   it('calculates sum of two negative numbers', () => {
-    expect(calculateNumber('SUM', -1.8, -2.0), -4);
+    expect(calculateNumber('SUM', -1.8, -2.0)).to.equal(-4);
   });
 
   it('calculates sum of one positive and one negative number', () => {
-    expect(calculateNumber('SUM', -1.8, 2.0), 0);
+    expect(calculateNumber('SUM', -1.8, 2.0)).to.equal(0);
   });
 
   it('calculates sum of 0 and one negative number', () => {
-    expect(calculateNumber('SUM', 0, -2.0), -2);
+    expect(calculateNumber('SUM', 0, -2.0)).to.equal(-2);
   });
 
   it('calculates sum of 0 and one positive number', () => {
-    expect(calculateNumber('SUM', 0, 2.0), 2);
+    expect(calculateNumber('SUM', 0, 2.0)).to.equal(2);
   });
 });
 
 describe('#calculateNumber() with SUBTRACT', () => {
   it('calculates difference of two positive numbers', () => {
-    expect(calculateNumber('SUBTRACT', 4.8, 2.0), 3);
-    expect(calculateNumber('SUBTRACT', 2.0, 4.8), -3);
+    expect(calculateNumber('SUBTRACT', 4.8, 2.0)).to.equal(3);
+    expect(calculateNumber('SUBTRACT', 2.0, 4.8)).to.equal(-3);
   });
 
   it('calculates difference of two negative numbers', () => {
-    expect(calculateNumber('SUBTRACT', -1.8, -2.0), 0);
+    expect(calculateNumber('SUBTRACT', -1.8, -2.0)).to.equal(0);
   });
 
   it('calculates difference of one positive and one negative number', () => {
-    expect(calculateNumber('SUBTRACT', -1.8, 2.0), -4);
-    expect(calculateNumber('SUBTRACT', 2.0, -1.8), 4);
+    expect(calculateNumber('SUBTRACT', -1.8, 2.0)).to.equal(-4);
+    expect(calculateNumber('SUBTRACT', 2.0, -1.8)).to.equal(4);
   });
 
   it('calculates difference of 0 and one negative number', () => {
-    expect(calculateNumber('SUBTRACT', 0, -2.0), 2);
-    expect(calculateNumber('SUBTRACT', -2, 0), -2);
+    expect(calculateNumber('SUBTRACT', 0, -2.0)).to.equal(2);
+    expect(calculateNumber('SUBTRACT', -2, 0)).to.equal(-2);
   });
 
   it('calculates difference of 0 and one positive number', () => {
-    expect(calculateNumber('SUBTRACT', 0, 2.0), -2);
-    expect(calculateNumber('SUBTRACT', 2.0, 0), 2);
+    expect(calculateNumber('SUBTRACT', 0, 2.0)).to.equal(-2);
+    expect(calculateNumber('SUBTRACT', 2.0, 0)).to.equal(2);
   });
 });
 
 describe('#calculateNumber() with DIVIDE', () => {
   it('calculates quotient of two positive numbers', () => {
-    expect(calculateNumber('DIVIDE', 4.8, 2.0), 2.5);
+    expect(calculateNumber('DIVIDE', 4.8, 2.0)).to.equal(2.5);
   });
 
   it('calculates quotient of two negative numbers', () => {
-    expect(calculateNumber('DIVIDE', -1.8, -2.0), 1);
+    expect(calculateNumber('DIVIDE', -1.8, -2.0)).to.equal(1);
   });
 
   it('calculates quotient with a positive dividend and a negative divisor', () => {
-    expect(calculateNumber('DIVIDE', 2.0, -1.8), -1);
+    expect(calculateNumber('DIVIDE', 2.0, -1.8)).to.equal(-1);
   });
 
   it('calculates quotient with negative dividend and positive divisor', () => {
-    expect(calculateNumber('DIVIDE', -1.8, 2.0), -1);
+    expect(calculateNumber('DIVIDE', -1.8, 2.0)).to.equal(-1);
   });
 
   it('calculates quotient of 0 dividend and negative divisor', () => {
-    expect(calculateNumber('DIVIDE', 0, -2.0), 0);
+    expect(calculateNumber('DIVIDE', 0, -2.0)).to.equal(0);
   });
 
   it('returns Error on division by 0', () => {
-    expect(calculateNumber('DIVIDE', 2.0, 0), 'Error');
+    expect(calculateNumber('DIVIDE', 2.0, 0)).to.equal('Error');
   });
 });
