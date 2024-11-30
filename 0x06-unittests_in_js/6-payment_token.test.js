e618ddeabd43164ebde69679f92977e3735db1b1
NiniolaX <loveafinni16@gmail.com>
2024-11-30 05:17:08 +0100

    Task 6: Added tests for function fail

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -1,11 +1,18 @@
 const { expect } = require('chai');
+
 const getPaymentTokenFromAPI = require('./6-payment_token');
 
 describe('getPaymentTokenFromAPI', () => {
-  it('returns resolved promise with success(bool)', () => {
+  it('returns resolved promise on success (true)', function(done) {
     getPaymentTokenFromAPI(true)
       .then((result) => {
         expect(result.data).to.equal('Successful response from the API');
-      })
-  })
-})
+        done();
+      });
+  });
+
+  it('returns undefined on fail (false)', () => {
+    const result = getPaymentTokenFromAPI(false);
+    expect(result).to.be.undefined;
+  });
+});
