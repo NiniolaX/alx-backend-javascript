c4da4977466270fd2888a98fd64e3a3aae8da246
NiniolaX <loveafinni16@gmail.com>
2024-11-30 06:34:11 +0100

    Task 0 and 1: Use assert instead of expect in tests

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -3,74 +3,74 @@ const calculateNumber = require('./1-calcul');
 
 describe('#calculateNumber() with SUM', () => {
   it('calculates sum of two positive numbers', () => {
-    expect(calculateNumber('SUM', 1.8, 2.0), 4);
+    assert.equal(calculateNumber('SUM', 1.8, 2.0), 4);
   });
 
   it('calculates sum of two negative numbers', () => {
-    expect(calculateNumber('SUM', -1.8, -2.0), -4);
+    assert.equal(calculateNumber('SUM', -1.8, -2.0), -4);
   });
 
   it('calculates sum of one positive and one negative number', () => {
-    expect(calculateNumber('SUM', -1.8, 2.0), 0);
+    assert.equal(calculateNumber('SUM', -1.8, 2.0), 0);
   });
 
   it('calculates sum of 0 and one negative number', () => {
-    expect(calculateNumber('SUM', 0, -2.0), -2);
+    assert.equal(calculateNumber('SUM', 0, -2.0), -2);
   });
 
   it('calculates sum of 0 and one positive number', () => {
-    expect(calculateNumber('SUM', 0, 2.0), 2);
+    assert.equal(calculateNumber('SUM', 0, 2.0), 2);
   });
 });
 
 describe('#calculateNumber() with SUBTRACT', () => {
   it('calculates difference of two positive numbers', () => {
-    expect(calculateNumber('SUBTRACT', 4.8, 2.0), 3);
-    expect(calculateNumber('SUBTRACT', 2.0, 4.8), -3);
+    assert.equal(calculateNumber('SUBTRACT', 4.8, 2.0), 3);
+    assert.equal(calculateNumber('SUBTRACT', 2.0, 4.8), -3);
   });
 
   it('calculates difference of two negative numbers', () => {
-    expect(calculateNumber('SUBTRACT', -1.8, -2.0), 0);
+    assert.equal(calculateNumber('SUBTRACT', -1.8, -2.0), 0);
   });
 
   it('calculates difference of one positive and one negative number', () => {
-    expect(calculateNumber('SUBTRACT', -1.8, 2.0), -4);
-    expect(calculateNumber('SUBTRACT', 2.0, -1.8), 4);
+    assert.equal(calculateNumber('SUBTRACT', -1.8, 2.0), -4);
+    assert.equal(calculateNumber('SUBTRACT', 2.0, -1.8), 4);
   });
 
   it('calculates difference of 0 and one negative number', () => {
-    expect(calculateNumber('SUBTRACT', 0, -2.0), 2);
-    expect(calculateNumber('SUBTRACT', -2, 0), -2);
+    assert.equal(calculateNumber('SUBTRACT', 0, -2.0), 2);
+    assert.equal(calculateNumber('SUBTRACT', -2, 0), -2);
   });
 
   it('calculates difference of 0 and one positive number', () => {
-    expect(calculateNumber('SUBTRACT', 0, 2.0), -2);
-    expect(calculateNumber('SUBTRACT', 2.0, 0), 2);
+    assert.equal(calculateNumber('SUBTRACT', 0, 2.0), -2);
+    assert.equal(calculateNumber('SUBTRACT', 2.0, 0), 2);
   });
 });
 
 describe('#calculateNumber() with DIVIDE', () => {
   it('calculates quotient of two positive numbers', () => {
-    expect(calculateNumber('DIVIDE', 4.8, 2.0), 2.5);
+    assert.equal(calculateNumber('DIVIDE', 4.8, 2.0), 2.5);
   });
 
   it('calculates quotient of two negative numbers', () => {
-    expect(calculateNumber('DIVIDE', -1.8, -2.0), 1);
+    assert.equal(calculateNumber('DIVIDE', -1.8, -2.0), 1);
   });
 
   it('calculates quotient with a positive dividend and a negative divisor', () => {
-    expect(calculateNumber('DIVIDE', 2.0, -1.8), -1);
+    assert.equal(calculateNumber('DIVIDE', 2.0, -1.8), -1);
   });
 
   it('calculates quotient with negative dividend and positive divisor', () => {
-    expect(calculateNumber('DIVIDE', -1.8, 2.0), -1);
+    assert.equal(calculateNumber('DIVIDE', -1.8, 2.0), -1);
   });
 
   it('calculates quotient of 0 dividend and negative divisor', () => {
-    expect(calculateNumber('DIVIDE', 0, -2.0), 0);
+    assert.equal(calculateNumber('DIVIDE', 0, -2.0), 0);
   });
 
   it('returns Error on division by 0', () => {
-    expect(calculateNumber('DIVIDE', 2.0, 0), 'Error');
+    assert.equal(calculateNumber('DIVIDE', 2.0, 0), 'Error');
   });
 });
